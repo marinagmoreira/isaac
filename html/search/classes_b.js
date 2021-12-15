86d02f49d2332fe875b1cf3ac032d5ab714e16fe
Marina Moreira <67443181+marinagmoreira@users.noreply.github.com>
2021-12-15 00:56:50 +0000

    Automatic update for d9083f9328c87b0c63072c5e3e626d97b244a357.

--- html/search/classes_b.js
+++ html/search/classes_b.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['refcamerror_175',['RefCamError',['../structdense__map_1_1RefCamError.html',1,'dense_map']]],
-  ['refdeptherror_176',['RefDepthError',['../structdense__map_1_1RefDepthError.html',1,'dense_map']]],
-  ['rfidmappernode_177',['RFIDMapperNode',['../classrfid__mapper_1_1RFIDMapperNode.html',1,'rfid_mapper']]],
-  ['rosbaghandle_178',['RosBagHandle',['../structdense__map_1_1RosBagHandle.html',1,'dense_map']]]
+  ['rfidmappernode_169',['RFIDMapperNode',['../classrfid__mapper_1_1RFIDMapperNode.html',1,'rfid_mapper']]],
+  ['rosbaghandle_170',['RosBagHandle',['../structdense__map_1_1RosBagHandle.html',1,'dense_map']]]
 ];
